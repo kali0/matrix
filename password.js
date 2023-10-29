fa5495f0f1b8b6b6c06155e2aef53ea1770bb626
Kali0 <dariovlopes@gmail.com>
2023-10-29 08:00:31 -0300

    first commit

---
+++ password.js
@@ -0,0 +1,10 @@
+// password.js
+function generate() {
+    var length = 12,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById('password').value = retVal;
+}
